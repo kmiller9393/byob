610ea3005348f15b9b6a03e018aafbaff03d7c5a
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-09 17:01:31 -0600

    Scrape web

--- back-end/web-scraping/web-scraper.js
+++ back-end/web-scraping/web-scraper.js
@@ -4,24 +4,6 @@ const fs = require('fs');
 let urls = [];
 
 // nightmare
-//   .goto('https://www.builtincolorado.com/jobs')
-//   .end()
-//   .type('#edit-search-api-fulltext', 'front end developer')
-//   .click('#edit-submit-jobs')
-//   .wait(5000)
-//   .evaluate(() => {
-//     const jobLinks = [...document.querySelectorAll('.wrap-view-page a')];
-
-//     return jobLinks.map(link => {
-//       return link.href;
-//     });
-//   })
-//   .then(result => {
-//     console.log('Everything works', result);
-//   })
-//   .catch(error => console.log('something went wrong', error));
-
-// nightmare
 //   .goto('https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=softw&sc.keyword=Software+Developer&locT=C&locId=1148170&jobType=')
 //   .end()
 //   .evaluate(() => {
