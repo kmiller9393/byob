da8d3fd04f9d53c7fdd4f2e4ac02235f64c0a8b7
Kurt Miller <kmiller9393@gmail.com>
2018-10-10 13:51:57 -0600

    Update webscraper.js

--- back-end/web-scraping/web-scraper.js
+++ back-end/web-scraping/web-scraper.js
@@ -31,23 +31,23 @@ const getJobData = async url => {
       const job_title = jobWrap.querySelector('h2').innerText;
       const headerWrap = document.querySelector('.logoHeader');
       const companyWrapper = headerWrap.querySelector('.empHeader');
-      const companyAndLocation = Array.from(companyWrapper.querySelectorAll('span'));
+      const companyAndLocation = Array.from(
+        companyWrapper.querySelectorAll('span')
+      );
       const company = companyAndLocation[0].innerText;
       const location = companyAndLocation[1].innerText;
-      const description = document.querySelector('.jobDescriptionContent').innerText
-      return {job_title, company, location, description};
+      const description = document.querySelector('.jobDescriptionContent')
+        .innerText;
+      return { job_title, company, location, description };
     })
     .end()
     .then(data => {
-      return data
+      return data;
     })
     .catch(error => {
-      console.log(error)
-    }) 
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+      console.log(error);
+    });
+  return result;
 };
 
 const cleanData = async () => {
@@ -56,13 +56,12 @@ const cleanData = async () => {
     const jobs = urls.reduce(async (acc, url) => {
       let allJobData = await acc;
       let jobData = await getJobData(url);
-        allJobData = [...allJobData, jobData]
-        return allJobData;
+      allJobData = [...allJobData, jobData];
+      return allJobData;
     }, Promise.resolve([]));
     return jobs;
-
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
@@ -71,8 +70,8 @@ const jobs = cleanData();
 // console.log(jobs)
 
 jobs.then(data => {
-  console.log(data)
-})
+  console.log(data);
+});
 // const getJob = async () => {
 //   const job = await getJobData('https://www.glassdoor.com/job-listing/software-developer-bctech-JV_IC1148170_KO0,18_KE19,25.htm?jl=2925659032&ctt=1539134670798')
 //   return job
