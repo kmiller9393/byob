ab41b344253b68567aca7f5b3b61b2cfd4e4f9a7
Kurt Miller <kmiller9393@gmail.com>
2018-10-10 18:14:23 -0600

    Add test environment data to knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -11,6 +11,18 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/job_board_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
